0be71f6df768992d12b95a270007761f84b4bf41
arognoni <alessandro.rognoni333@gmail.com>
2026-04-24 21:57:49 +0200

    delete source code

--- Home.py
+++
@@ -1,9 +0,0 @@
-"""Dashboard home page."""
-
-import streamlit as st
-
-st.set_page_config(page_title="Home", page_icon="static/favicon-32x32.png")
-st.logo("static/uniupo-logo.svg", size="large")
-st.title("Home")
-
-st.markdown("Landing page content")
